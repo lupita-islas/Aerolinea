22d0e22c4352219b614a0b5823705a944d2df751
lupita-islas <rgio.11.96@gmail.com>
2016-11-28 22:02:16 -0600

    avance lunes

--- verificar.php
+++ verificar.php
@@ -30,6 +30,8 @@ echo $menor;
 echo $total_bol;
 echo '<br>';
 
+$vuelos;
+$f=0;
 
 if($conn->connect_error) {
     die("Error en la conexion".$conn->connect_error);
@@ -44,16 +46,49 @@ if($conn->connect_error) {
         }else{
             $sql1="SELECT * from vuelo where Origen='$origen' and Destino='$destino' and fecha_salida BETWEEN '$sal_bef' and '$sal_af'";
             $resultado=$conn->query($sql1);
+            //Resultado de vuelos
             if($resultado->num_rows>0) {
                 while($row = $resultado->fetch_assoc()) {
-                    echo $row["Origen"];
-                    echo $row["Destino"];
-                    echo $row["id"];
-                    echo $row["Fecha_salida"];
-                    echo $row["Hora_salida"];
+                    $vuelos[$f][0]=$row["Id"];
+                    $vuelos[$f][1]=$row["Origen"];
+                    $vuelos[$f][2]=$row["Destino"];
+                    $vuelos[$f][3]=$row["Fecha_salida"];
+                    $vuelos[$f][4]=$row["Hora_salida"];
+                    $vuelos[$f][5]=$row["Fecha_llegada"];
+                    $vuelos[$f][6]=$row["Hora_llegada"];
+                    $vuelos[$f][7]="NO"; //seleccionado
+                    $vuelos[$f][8]="SI"; //disponible_turista
+                    $vuelos[$f][9]="SI";//disponible_primera
+
+                    echo $vuelos[$f][0];
+                    echo $vuelos[$f][1];
+                    echo $vuelos[$f][2];
+                    echo $vuelos[$f][3];
+                    echo $vuelos[$f][4];
                     echo '<br>';
+                    $f+=1;
 
                 }
+
+                $turista=0;
+                $primera=0;
+
+                for($x=0; $x<$f; $x++){
+                    $id=$vuelos[$x][0];
+                    $sql_dis="SELECT * from asientos where Id_vuelo='$id'";
+                    $res_dis=$conn->query($sql_dis);
+                    if($res_dis->num_rows>=52){
+                        if($res['Clase']=="PRIMERA"){
+                            $primera++;
+                        }else{
+                            $turista++;
+                        }
+                        $vuelos[$x][7]="NO";
+                    }
+                }
+
+                mostrar();
+
             }else{
                 ?> <script>
                     alert('No se encontro ningún vuelo ');
@@ -72,5 +107,36 @@ if($conn->connect_error) {
     }
 }
 
+function mostrar(){
+    global $vuelos;
+    ?>
+    <table style="border:solid 1px black " align="center" cellspacing="10px" id="basic-table">
+            <tr>
+                <th></th>
+                <th>OBJETO</th>
+                <th>CANTIDAD</th>
+                <th>FECHA</th>
+                <th>COMPRADO</th>
+            </tr>
+        <?php
+}
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
 
 ?>
